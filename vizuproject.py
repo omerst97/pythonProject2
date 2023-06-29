35a8704219909468d8a1a5f93af6f66916f38ff8
automaticanalyst <automaticanalyst@gmail.com>
2023-06-29 12:12:10 +0300

    commited

--- vizuproject.py
+++ vizuproject.py
@@ -228,7 +228,7 @@ st.subheader('UNFCCC Agreement VS Reality')
 st.text('This graph shows the impact of the United Nations Framework Convention on Climate '+"\n"+'Change (UNFCCC). '
         ' This committee, signed in year 1990 by 154 countries. each country '+"\n"+'sighed that '
         ' its emissions will not exceed the emissions in year 1990. '+"\n"+
-        'We used a machine learning algorithm to predict the emissions of each country in the'+"\n"+''
+        ' We used a machine learning algorithm to predict the emissions of each country in the'+"\n"+''
         'year 2020 if it had continued on a similar trend from 1960 to 1990. Based on this,'+"\n"+
         'we can estimate the impact of this committee on the emissions level of different '+"\n"+'countries.'
 'The following graph allows us to investigate the impact of the committee \non all countries and regions around the world.')
